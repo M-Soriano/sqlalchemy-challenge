7230ddedb78f0902505f0496574b9cba1da36cb4
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-25 02:49:45 -0800

    In app.py, added dependencies and added information for database setup

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 # Import the dependencies.
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 
 #################################################
 # Database Setup
 #################################################
+engine= create_engine('sqlite:///hawaii.sqlite')
 
 
 # reflect an existing database into a new model
-
+Base= automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurements=Base.classes.measurement
+stations=Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session=Session(engine)
 
 #################################################
 # Flask Setup
